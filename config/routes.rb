cf76704bc1eeeac4d4df164caf6bcabc4edc3141
Rosy Arreola <rosy.arreola@yahoo.com>
2019-12-06 23:04:19 +0000

    before tags controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'static_pages#index'
-  resources :sections, only: [:index, :new, :create, :show]
+  resources :sections
+  resources :tags, only: [:index, :show]
 end

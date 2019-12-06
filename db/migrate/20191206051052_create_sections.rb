34888d749a3ccbef4aa639ebd84b592a7eb5794f
Rosy Arreola <rosy.arreola@yahoo.com>
2019-12-06 06:08:14 +0000

    cards

--- db/migrate/20191206051052_create_sections.rb
+++ db/migrate/20191206051052_create_sections.rb
@@ -2,6 +2,7 @@ class CreateSections < ActiveRecord::Migration[5.2]
   def change
     create_table :sections do |t|
       t.string :title
+      t.string :description
       t.timestamps
     end
   end
